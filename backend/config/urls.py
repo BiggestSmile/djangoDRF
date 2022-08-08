2caca745229e219346453024a1fcd14d40d925fe
Egor <egorsilin@gmail.com>
2022-08-08 11:22:22 +0300

    lesson content

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from library.views import AuthorModelViewSet
+
+router = DefaultRouter()
+router.register('authors', AuthorModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
