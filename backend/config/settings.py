9cb67304d0f17e33828073f75683008663b153df
Egor <egorsilin@gmail.com>
2022-08-11 15:03:27 +0300

    lesson_02 program is ok

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'library',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import AuthorList from './components/AuthorList.js'
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            'authors': []
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://127.0.0.1:8008/api/authors/')
+            .then(response => {
+                const authors = response.data
+                this.setState(
+                    {
+                        'authors': authors
+                    }
+                )
+            })
+            .catch(error => console.log(error))
+    }
+
+    render() {
+        return (
+            <div>
+                <AuthorList authors={this.state.authors}/>
+            </div>
+        )
+    }
 }
 
 export default App;
