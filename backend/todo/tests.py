f638377eef87fb176d5a6d424cb48c44621e455a
Egor <egorsilin@gmail.com>
2022-09-05 12:29:12 +0300

    added APIClient tests examples

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -10,4 +10,38 @@ from library.models import CustomUser
 from todo.views import ProjectModelViewSet, ToDoUserModelViewSet
 from todo.models import Project, ToDo
 
+
 # Create your tests here.
+class TestProjectModelViewSet(TestCase):
+    def test_get_detail(self):
+        project = mixer.blend(Project)
+        client = APIClient()
+        admin = CustomUser.objects.create_superuser('admin', 'admin@admin.com',
+                                                    'admin123456')
+        client.login(username='admin', password='admin123456')
+
+        response = client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        client.logout()
+
+    def test_put_detail(self):
+        client = APIClient()
+        admin = CustomUser.objects.create_superuser('admin', 'admin@admin.com',
+                                                    'admin123456')
+        # project = Project.objects.create({'name': 'TestProject_XXX',
+        #                                   'repo_link': 'https://TestProject_XXX.git.com',
+        #                                   'users': admin,
+        #                                   })
+        project = mixer.blend(Project)
+        client.login(username='admin', password='admin123456')
+        response = client.put(f'/api/projects/{project.id}/', {'name': 'TestProject',
+                                                               'repo_link': 'https://TestProject.git.com',
+                                                               # 'users': admin,
+                                                               }
+                              )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        project = Project.objects.get(id=project.id)
+        self.assertEqual(project.name, 'TestProject')
+        self.assertEqual(project.repo_link, 'https://TestProject.git.com')
+        # self.assertEqual(project.users, admin)
+        client.logout()
