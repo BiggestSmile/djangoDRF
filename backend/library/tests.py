2e8d0d71a3904271296695b9b92280109d42698a
Egor <egorsilin@gmail.com>
2022-09-05 11:11:25 +0300

    added APIRequestFactory tests examples

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -11,4 +11,21 @@ from todo.views import ProjectModelViewSet, ToDoUserModelViewSet
 from todo.models import Project, ToDo
 
 
-# Create your tests here.
+class TestCustomUserModelViewSet(TestCase):
+
+    def test_get_list_without_auth(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/custom-users/')
+        view = CustomUserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_list_with_auth(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/custom-users/')
+        admin = CustomUser.objects.create_superuser('admin', 'admin@admin.com',
+                                                    'admin123456')
+        force_authenticate(request, admin)
+        view = CustomUserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
