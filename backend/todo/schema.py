b3bd20140a79f6d0dfad19fadd778f498ff193e3
Egor <egorsilin@gmail.com>
2022-09-12 12:38:30 +0300

    finished lesson_10

--- backend/todo/schema.py
+++ backend/todo/schema.py
@@ -40,5 +40,23 @@ class Query(graphene.ObjectType):
     def resolve_all_users(root, info):
         return CustomUser.objects.all()
 
+    user_by_id = graphene.Field(CustomUserType, id=graphene.Int(required=True))
+
+    def resolve_user_by_id(self, info, id):
+        try:
+            return CustomUser.objects.get(id=id)
+
+        except CustomUser.DoesNotExist:
+            return None
+
+    todos_by_project_name = graphene.List(TodoType, name=graphene.String(required=False))
+
+    def resolve_todos_by_project_name(self, info, name=None):
+        todos = ToDo.objects.all()
+
+        if name:
+            todos = todos.filter(project__name=name)
+        return todos
+
 
 schema = graphene.Schema(query=Query)
