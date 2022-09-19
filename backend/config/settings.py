a67dd4b20f3e63cbee7369a0f5deaf708270e54c
EgorSilin <EgorSilin@gmail.com>
2022-09-19 17:10:11 +0300

    finished lesson_12

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6nvub@78uut_8_roh6xxt=enzh+))h@=&a-jj-ee-@@r)gijf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = bool(os.environ.get('DEBUG', default=False))
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
